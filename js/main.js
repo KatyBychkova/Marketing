a5033bedda2f78ceac195048e9c6c02379e69524
unknown <you@example.com>
2021-07-11 15:10:37 +0300

    slider complite

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(function(){
       duration: 300
 
   }});
+
   $('[data-fancybox]').fancybox({
     youtube : {
         controls : 1,
@@ -14,10 +15,12 @@ $(function(){
    });
 
 
+  $('.slider-blog__inner').slick({
+    dots : true,
+    prevArrow : '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow : '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
+  });
 
-
-
-
-  
+ 
 });
 
