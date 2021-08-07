dfe38d5e5231a4bc6e5198be42446d920a46dac7
Katya_Fedorovka <kats@list.ru>
2021-08-07 13:07:39 +0300

    about adaptive complite

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 $(function(){
 
-  var mixer = mixitup('.portfolio__content', {
-    animation: {
-      duration: 300
-
-  }});
-
+  
   $('[data-fancybox]').fancybox({
     youtube : {
-        controls : 1,
-        showinfo : 1,
-        autoplay : true
+      controls : 1,
+      showinfo : 1,
+      autoplay : true
     },
-   });
-
-
+  });
+  
+  
   $('.slider-blog__inner').slick({
     dots : true,
     prevArrow : '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
     nextArrow : '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
   });
 
+  $('.menu__btn').on('click',function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+
+
+
+
+  
+  var mixer = mixitup('.portfolio__content', {
+    animation: {
+      duration: 300
+
+  }});
  
 });
 
