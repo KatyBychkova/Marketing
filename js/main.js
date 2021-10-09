0bcbc0bc37b1f93a06885bdb7e601a7cb9cd81bb
unknown <you@example.com>
2021-10-09 11:20:24 +0300

    slider cpmplite

--- js/main.js
+++ js/main.js
@@ -13,7 +13,15 @@ $(function(){
   $('.slider-blog__inner').slick({
     dots : true,
     prevArrow : '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
-    nextArrow : '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
+    nextArrow : '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+           arrows: false,
+        }
+      }
+    ]
   });
 
   $('.menu__btn').on('click',function(){
