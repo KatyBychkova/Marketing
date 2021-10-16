e8c19d786b1b817875710dfd5ca1128b8194df34
unknown <you@example.com>
2021-10-16 19:44:18 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+  $(".menu a, .go-top").on("click", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
   
   $('[data-fancybox]').fancybox({
     youtube : {
@@ -24,7 +38,7 @@ $(function(){
     ]
   });
 
-  $('.menu__btn').on('click',function(){
+  $('.menu__btn, .menu a').on('click',function(){
     $('.header__top-inner').toggleClass('header__top-inner--active');
   });
 
